ecca031bd68563ab57c92c6bad5b7b0cb2925bba
Taras Dorenskiy <tdorenskiy@gmail.com>
2021-12-22 02:12:14 +0300

    db connection

--- db.py
+++ db.py
@@ -1,9 +1,25 @@
-from PyQt5.QtSql import QSqlDatabase
+from PyQt5.QtSql import QSqlDatabase, QSqlQueryModel, QSqlQuery
+# from PyQt5.QWidgets import QTableView, QApplication
+import sys
 
-db = QSqlDatabase.addDatabase("QODBC")
-db.setDatabaseName(
-"DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=D:\\Library.accdb")
-if db.open(): 
-	print("opened")
-else:
-	print("not")
+SERVER_NAME = 'LAPTOP-T9H1AR8N\SQLEXPRESS'
+DATABASE_NAME = 'DBLibrary'
+USERNAME = ''
+PASSWORD = ''
+
+def createConnection():
+        connString = f'DRIVER={{SQL Server}};'\
+                     f'SERVER={SERVER_NAME};'\
+                     f'DRIVER={DATABASE_NAME};'
+
+        global db
+        db = QSqlDatabase.addDatabase("QODBC")
+        db.setDatabaseName(connString)
+        if db.open(): 
+                print("opened")
+                return True
+        else:
+                print('false')
+                return False
+        
+createConnection()
