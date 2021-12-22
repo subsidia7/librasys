1be47570971c914c0f072cfa92af6a8447fe2e6b
Taras Dorenskiy <tdorenskiy@gmail.com>
2021-12-22 14:57:57 +0300

    sqlite

--- db.py
+++ db.py
@@ -1,25 +1,35 @@
-from PyQt5.QtSql import QSqlDatabase, QSqlQueryModel, QSqlQuery
-# from PyQt5.QWidgets import QTableView, QApplication
-import sys
+# from PyQt5.QtSql import QSqlDatabase, QSqlQueryModel, QSqlQuery
+# from PyQt5.QtWidgets import QTableView, QApplication
+# import sys
 
-SERVER_NAME = 'LAPTOP-T9H1AR8N\SQLEXPRESS'
-DATABASE_NAME = 'DBLibrary'
-USERNAME = ''
-PASSWORD = ''
+# SERVER_NAME = 'LAPTOP-T9H1AR8N\SQLEXPRESS'
+# DATABASE_NAME = 'DBLibrary'
+# USERNAME = ''
+# PASSWORD = ''
 
-def createConnection():
-        connString = f'DRIVER={{SQL Server}};'\
-                     f'SERVER={SERVER_NAME};'\
-                     f'DRIVER={DATABASE_NAME};'
+# def createConnection():
+#         connString = f'DRIVER={{SQL Server}};'\
+#                      f'SERVER={SERVER_NAME};'\
+#                      f'DRIVER={DATABASE_NAME};'
 
-        global db
-        db = QSqlDatabase.addDatabase("QODBC")
-        db.setDatabaseName(connString)
-        if db.open(): 
-                print("opened")
-                return True
-        else:
-                print('false')
-                return False
-        
-createConnection()
+#         global db
+#         db = QSqlDatabase.addDatabase("QODBC")
+#         db.setDatabaseName(connString)
+#         if db.open(): 
+#                 print("opened")
+#                 return True
+#         else:
+#                 print('false')
+#                 return False
+
+# if __name__ == "__main__":
+#         app = QApplication(sys.argv)
+#         createConnection()
+
+import sqlite3 as sq
+from PyQt5.QtSql import QSqlDatabase
+con = QSqlDatabase.addDatabase("QSQLITE")
+con.setDatabaseName("dblibrary")
+# Open the connection
+if con.open():
+        print("УРА БЛЯ")
